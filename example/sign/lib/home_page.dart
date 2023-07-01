795e779942c3d2ae6c3e8d583743698721e16e82
Sterling Long <sterling.long@lztek.io>
2023-07-01 11:34:22 -0600

    Updated recommended wallets

--- example/sign/lib/home_page.dart
+++ example/sign/lib/home_page.dart
@@ -36,7 +36,6 @@ class _MyHomePageState extends State<MyHomePage> {
       recommendedWalletIds: {
         'afbd95522f4041c71dd4f1a065f971fd32372865b416f95a0b1db759ae33f2a7',
         '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
-        // 'c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a',
         'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
         // '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369' // Defiant
       },
